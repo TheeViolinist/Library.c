402048b19663a5000a5b2902a1c3b9115086acbe
Leandro Lucas <LeoViolinist22@gmail.com>
2021-09-23 12:03:55 -0300

    Fineshed the change of the lenguage

--- biblioteca.c
+++ biblioteca.c
@@ -7,34 +7,34 @@
 
 // library book database
 int openFile(char *nameOfMatter, int counter, int codeMatter);
-int retiradaDeLivro(char *chosenBook, int withdrawalLine, int codeMatter);
+int bookWithdrawal(char *chosenBook, int withdrawalLine, int codeMatter);
 
 typedef struct
 {
     char writer[MAX];
     char content[MAX];
-    char nome[MAX];
+    char name[MAX];
 
-} dados;
+} data;
 
 typedef struct
 {
-    dados dados;
+    data data;
 
-} materia;
+} material;
 
-materia descricao[MAX];
+material description[MAX];
 
 //codeMatter
-// 0 = matematica
-// 1 = geografia
-// 2 = historia
-// 3 = sociologia
-// 4 = fisica
-// 5 = quimica
-// 6 = sociologia
-// 7 = estudos gerais
-// 8 = teologia
+// 0 = math
+// 1 = geography
+// 2 = history
+// 3 = sociology
+// 4 = physics
+// 5 = chemistry
+// 6 = general studies
+// 7 = theology
+
 
 void clean(void);
 
@@ -42,77 +42,77 @@ int main(void)
 {
     FILE *math;
     FILE *livros;
-    int escolhaDoUsuario, tamanhoDoNome;
-    char nomeDoUsuario[MAX];
-    int quantidadeDeLivros, adicaoDeLivro, codeMatter, resetDeLivro;
+    int userChoice, nameSize;
+    char nameUser[MAX];
+    int numberBooks, bookAdd, codeMatter, restartBooks;
     int counter = 0;
     int withdrawalLine;
-    char *livroRetirado[MAX];
+    char *withdrawnBook[MAX];
 
-    printf("Voce deseja adicionar um novo livro na biblioteca? [1] Sim ou [2] Nao.\n");
-    scanf("%d", &adicaoDeLivro);
-    printf("Voce deseja resetar os livros? [1] Sim ou [2] Nao\n");
-    scanf("%d", &resetDeLivro);
-    if (resetDeLivro == 1)
+    printf("Do you wanna adding a book to library? [1] Yes or [2] No.\n");
+    scanf("%d", &bookAdd);
+    printf("Do you wanna reset the books? [1] Yes or [2] No\n");
+    scanf("%d", &restartBooks);
+    if (restartBooks == 1)
     {
-        printf("Digite o código da matéria que você quer resetar: ");
+        printf("Enter the code of the materiall you want to reset: ");
         scanf("%d", &codeMatter);
         if (codeMatter == 0)
         {
             math = fopen("math.csv", "w");
             math = fopen("math.csv", "a");
-            fprintf(math, "%s, %s, %s\n", "nome", "writer", "content");
+            fprintf(math, "%s, %s, %s\n", "name", "writer", "content");
             fclose(math);
         }
 
         return 0;
     }
 
-    if (adicaoDeLivro == 1)
+    if (bookAdd == 1)
     {
-        //banco de livros
-        printf("Digite a quantidade de livros que voce quer armazenas.\n");
-        scanf("%d", &quantidadeDeLivros);
-        printf("Digite o código da matéria que você quer armazenar: ");
+        //data of books
+        printf("Enter the number of books you want to store.\n");
+        scanf("%d", &numberBooks);
+        printf("Enter the code of the materiall you want to add: ");
         scanf("%d", &codeMatter);
         system("clear");
 
         getchar();
-        for (int i = 0; i < quantidadeDeLivros; i++)
+        for (int i = 0; i < numberBooks; i++)
         {
             if (codeMatter == 0)
             {
                 math = fopen("math.csv", "a");
             }
 
-            printf("Digite o nome do livro.\n");
-            fgets(descricao[codeMatter].dados.nome, MAX, stdin);
-            tamanhoDoNome = strlen(descricao[codeMatter].dados.nome);
-            if (descricao[codeMatter].dados.nome[tamanhoDoNome - 1] == '\n')
+            printf("Enter the name of the book.\n");
+            fgets(description[codeMatter].data.name, MAX, stdin);
+            nameSize = strlen(description[codeMatter].data.name);
+            if (description[codeMatter].data.name[nameSize - 1] == '\n')
             {
-                descricao[codeMatter].dados.nome[tamanhoDoNome - 1] = '\0';
+                description[codeMatter].data.name[nameSize - 1] = '\0';
             }
 
-            printf("Digite o writer do livro.\n");
-            fgets(descricao[codeMatter].dados.writer, MAX, stdin);
-            tamanhoDoNome = strlen(descricao[codeMatter].dados.writer);
-            if (descricao[codeMatter].dados.writer[tamanhoDoNome - 1] == '\n')
+            printf("Enter the writer of the book.\n");
+            fgets(description[codeMatter].data.writer, MAX, stdin);
+            nameSize = strlen(description[codeMatter].data.writer);
+            if (description[codeMatter].data.writer[nameSize - 1] == '\n')
             {
-                descricao[codeMatter].dados.writer[tamanhoDoNome - 1] = '\0';
+                description[codeMatter].data.writer[nameSize - 1] = '\0';
             }
 
-            printf("Digite o content do livro.\n");
-            fgets(descricao[codeMatter].dados.content, MAX, stdin);
-            tamanhoDoNome = strlen(descricao[codeMatter].dados.content);
-            if (descricao[codeMatter].dados.content[tamanhoDoNome - 1] == '\n')
+            printf("Enter the content of the book.\n");
+            fgets(description[codeMatter].data.content, MAX, stdin);
+            nameSize = strlen(description[codeMatter].data.content);
+            if (description[codeMatter].data.content[nameSize - 1] == '\n')
             {
-                descricao[codeMatter].dados.content[tamanhoDoNome - 1] = '\0';
+                description[codeMatter].data.content[nameSize - 1] = '\0';
             }
 
-            //armazenamento em matematica
+            //data of math
             if (codeMatter == 0)
             {
-                fprintf(math, "%s, %s, %s\n", descricao[codeMatter].dados.nome, descricao[codeMatter].dados.writer, descricao[codeMatter].dados.content);
+                fprintf(math, "%s, %s, %s\n", description[codeMatter].data.name, description[codeMatter].data.writer, description[codeMatter].data.content);
                 fclose(math);
             }
 
@@ -136,84 +136,85 @@ int main(void)
            " |                                         |\n"
            " |_________________________________________|\n");
 
-    printf("Digite uma tecla para avancar.\n");
+    printf("Enter the key to advance.\n");
     getchar();
 
     system("clear");
 
-    printf("Digite seu nome: ");
+    printf("Type your name: ");
 
-    fgets(nomeDoUsuario, MAX, stdin);
-    //retirada do espaço que o fgets
-    tamanhoDoNome = strlen(nomeDoUsuario);
-    if (nomeDoUsuario[tamanhoDoNome - 1] == '\n')
+    fgets(nameUser, MAX, stdin);
+    //withdrawnl from \n
+    nameSize = strlen(nameUser);
+    if (nameUser[nameSize - 1] == '\n')
     {
-        nomeDoUsuario[tamanhoDoNome - 1] = '\0';
+        nameUser[nameSize - 1] = '\0';
     }
     system("clear");
 
-    printf("Bem vindo %s a biblioteca Penbookers\n", nomeDoUsuario);
+    printf("Welcome %s to library Penbookers\n", nameUser);
     printf("\n");
 
-    printf("Escolha uma das opcoes abaixo\n"
-           "1 - Escolher um livro para estudar na biblioteca.\n"
-           "2 - Escolher um livro para levar.\n"
-           "3 - Saber quanto tempo falta para entrega do meu livro.\n"
-           "4 - Saber alguma informacao sobre o livro, qual é o writer e afins.\n");
-    printf("Digite o que voce deseja.\n");
-    scanf("%d", &escolhaDoUsuario);
+    printf("Choose one of the options below \n"
+           "1 - Choosing a book to study at the library.\n"
+           "2 - Choose a book to take.\n"
+           "3 - How long to return the book.\n"
+           "4 - Know some information about the book.\n");
+
+    printf("Type what do you want.\n");
+    scanf("%d", &userChoice);
     system("clear");
 
-    switch (escolhaDoUsuario)
+    switch (userChoice)
     {
     case 1:
 
-        printf("Veja os generos disponiveis:\n"
-               "1 - Livros de estudo.\n"
-               "2 - Livros de Romance.\n"
-               "3 - Livros de crianças.\n"
-               "4 - Livros de ficcao.\n");
+        printf("See the available genres:\n"
+               "1 - Books of study.\n"
+               "2 - Books of romance.\n"
+               "3 - Books for childrens.\n"
+               "4 - Books of fiction.\n");
 
-        printf("Digite o que voce deseja: ");
-        scanf("%d", &escolhaDoUsuario);
+        printf("Type what do you want: ");
+        scanf("%d", &userChoice);
 
         system("clear");
-        switch (escolhaDoUsuario)
+        switch (userChoice)
         {
         case 1:
-            printf("Escolha a disciplina:\n"
-                   "1 - Matemática. \n"
-                   "2 - Geografia.\n"
-                   "3 - História.\n"
-                   "4 - Sociologia.\n"
-                   "5 - Fisica.\n"
-                   "6 - Química.\n"
-                   "7 - Filosofia.\n"
-                   "8 - Estudos gerais.\n"
-                   "9 - Teologia.\n");
-            printf("Digite o que voce deseja:\n");
-            scanf("%d", &escolhaDoUsuario);
+            printf("Choose the subject:\n"
+                   "1 - Math. \n"
+                   "2 - Geography.\n"
+                   "3 - History.\n"
+                   "4 - Sociology.\n"
+                   "5 - Physics.\n"
+                   "6 - Chemistry.\n"
+                   "7 - Philosophy.\n"
+                   "8 - General studies.\n"
+                   "9 - Theology.\n");
+            printf("Type what do you want:\n");
+            scanf("%d", &userChoice);
             system("clear");
 
-            switch (escolhaDoUsuario)
+            switch (userChoice)
             {
             case 1:
                 codeMatter = 0;
 
-                //exposição dos livros de matematica
+                //Showing math books
                 for (int i = 0; i < M; i++)
                 {
-                    counter = openFile(descricao[i].dados.nome, counter, codeMatter);
-                    printf("%s\n", descricao[i].dados.nome);
+                    counter = openFile(description[i].data.name, counter, codeMatter);
+                    printf("%s\n", description[i].data.name);
                 }
-                printf("Voce deseja retirar algum livro? [1] SIM ou [2] NAO\n");
-                scanf("%d", &escolhaDoUsuario);
-                if (escolhaDoUsuario == 1)
+                printf("Do you want to withdraw a book? [1] Yes or [2] No\n");
+                scanf("%d", &userChoice);
+                if (userChoice == 1)
                 {
-                    printf("Digite o numero presente antes do livro para retirar:\n");
+                    printf("Enter the number present before the book to withdraw:\n");
                     scanf("%d", &withdrawalLine);
-                    retiradaDeLivro(livroRetirado, withdrawalLine, codeMatter);
-                    printf("Seu livro retirado foi: %s\n", livroRetirado);
+                    bookWithdrawal(withdrawnBook, withdrawalLine, codeMatter);
+                    printf("Your withdrawn book was: %s\n", withdrawnBook);
                 }
                 else
                 {
@@ -222,29 +223,29 @@ int main(void)
                 break;
             }
         case 2:
-            //exposição dos livros de geografia
+            //Showing geography books
 
             break;
         }
         break;
 
     case 2:
-        printf("Veja os generos disponiveis:\n"
-               "1 - Livros de estudo.\n"
-               "2 - Livros de Romance.\n"
-               "3 - Livros de crianças.\n"
-               "4 - Livros de ficcao.\n");
+        printf("See the available genres:\n"
+               "1 - Books of study.\n"
+               "2 - Books of romance.\n"
+               "3 - Books for childrens.\n"
+               "4 - Books of fiction.\n");
         break;
     }
 }
 
-// leitura do arquivo
+// File read
 int openFile(char *nameOfMatter, int counter, int codeMatter)
 {
-    int codigo = codeMatter;
+    int code = codeMatter;
     counter++;
     FILE *math;
-    if (codigo == 0)
+    if (code == 0)
     {
         math = fopen("math.csv", "r");
     }
@@ -253,16 +254,16 @@ int openFile(char *nameOfMatter, int counter, int codeMatter)
     int i = 0;
     do
     {
-        if (codigo == 0)
+        if (code == 0)
         {
-            //o fgets ler tudo ate achar um \n e armazena tudo em buffer
+            //fgets reads everything until it finds one \n and buffers everything
             fgets(buffer, MAX, math);
             fscanf(math, "%[^,]", nameOfMatter);
             i++;
         }
 
     } while (i < counter);
-    if (codigo == 0)
+    if (code == 0)
     {
         fclose(math);
     }
@@ -270,9 +271,9 @@ int openFile(char *nameOfMatter, int counter, int codeMatter)
     return counter;
 }
 
-//funcao para retirada de livro
+//book withdrawal function
 
-int retiradaDeLivro(char *chosenBook, int withdrawalLine, int codeMatter)
+int bookWithdrawal(char *chosenBook, int withdrawalLine, int codeMatter)
 {
     FILE *math;
     if (codeMatter == 0)
@@ -280,9 +281,9 @@ int retiradaDeLivro(char *chosenBook, int withdrawalLine, int codeMatter)
         math = fopen("math.csv", "r");
     }
     
-    FILE *retirada = fopen("transferindo.csv", "w");
+    FILE *withdrawal = fopen("transferring.csv", "w");
     char buffer[MAX];
-    int linhaAtual = 1;
+    int currentLine = 1;
     int counter = 0;
     
 
@@ -290,21 +291,22 @@ int retiradaDeLivro(char *chosenBook, int withdrawalLine, int codeMatter)
     {
         while (fgets(buffer, MAX, math) != NULL)
         {
-            if (linhaAtual != withdrawalLine)
+            if (currentLine != withdrawalLine)
             {
-                fputs(buffer, retirada);
+                fputs(buffer, withdrawal);
             }
-            //ajeitar a retirada de livro
+
+            
 
             strcpy(chosenBook, buffer);
             memset(buffer, 0, sizeof(char) * MAX);
-            linhaAtual += 1;
+            currentLine += 1;
         }
         fclose(math);
     }
     
   
-    //ele modifica o livro, para retirar somente o nome
+    //modifies the book, to remove only the name
     while (1)
     {
         if (chosenBook[counter] == ',')
@@ -315,8 +317,8 @@ int retiradaDeLivro(char *chosenBook, int withdrawalLine, int codeMatter)
         counter++;
     }
 
-    fclose(retirada);
+    fclose(withdrawal);
     remove("math.csv");
-    rename("transferindo.csv", "math.csv");
+    rename("transferring.csv", "math.csv");
     return 0;
 }
